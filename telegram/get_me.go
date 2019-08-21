7710f843e12489a0057a93f544d6c16c91934794
Ilya Manin <bkmzvf@gmail.com>
2019-08-21 15:54:55 +0700

    Return an object (not a pointer) in all Telegram API methods

--- telegram/get_me.go
+++ telegram/get_me.go
@@ -3,11 +3,11 @@ package telegram
 /*
 A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
 */
-func (c *Client) GetMe() (*User, error) {
+func (c *Client) GetMe() (User, error) {
 	var err error
 	var result User
 
 	err = c.performRequest("getMe", nil, nil, &result)
 
-	return &result, err
+	return result, err
 }
